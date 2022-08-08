e0126fbddb891b0a5199e84ed80dafd55d7d3787
JKawa <jolkawa91@gmail.com>
2022-08-08 18:11:55 +0200

    poprawka2

--- Connection.py
+++ Connection.py
@@ -3,12 +3,13 @@ from contextlib import contextmanager
 from psycopg2.pool import SimpleConnectionPool
 from dotenv import load_dotenv
 
-DATABASE_PROMPT = "Enter the DATABASE_URI value or leave empty to load from .env file: "
+DATABASE_PROMPT = "Enter the DATABASE_URI value : "
 #database_uri ='postgres://muqopthd:5-4ufrJrs89u6rxc-_uIa_SN427i8L92@tyke.db.elephantsql.com/muqopthd'
 database_uri = input(DATABASE_PROMPT)
 if not database_uri:
     load_dotenv()
     database_uri = os.environ["DATABASE_URI"]
+    os.environ['DATABASE_URL']='<URI>'
 
 pool = SimpleConnectionPool(minconn=1, maxconn=10, dsn=database_uri)
 
